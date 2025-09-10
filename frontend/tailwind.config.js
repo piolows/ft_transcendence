d04599654e6960aed1f7adc5861e8a353f2f3590
Pierce Polintan <pipolint@lab1r1s5.42abudhabi.ae>
2025-09-10 20:55:20 +0400

    added tailwind config file and styles.css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // this.corePlugins{
+  //   preflight: false,
+  // },
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
