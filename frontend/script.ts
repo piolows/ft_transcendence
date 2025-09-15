43eff8dbd33f0a330a19c35dd880d27dee9dd694
Pierce Polintan <pipolint@lab1r2s7.42abudhabi.ae>
2025-09-15 15:38:58 +0400

    added another paddle to the game with collisions.

--- frontend/script.ts
+++ frontend/script.ts
@@ -8,12 +8,34 @@ context.fillStyle = 'black';
 context.fillRect(0, 0, cv.width, cv.height);
 
 // circle variables
-let dY = 3;
-let dX = 3;
+let dY = 4;
+let dX = 4;
 let r = 15
 let x = r;
 let y = r;
 let style = 'white';
+
+class Paddle
+{
+	height: number;
+	width: number;
+	xPos: number;
+	yPos: number;
+	color: string;
+	up: boolean;
+	down: boolean;
+
+	constructor(h: number, w: number, x: number, y: number, c: string)
+	{
+		this.height = h;
+		this.width = w;
+		this.xPos = x;
+		this.yPos = y;
+		this.color = c;
+		this.up = false;
+		this.down = false;
+	}
+}
 
 // left paddle variables
 const paddleH = 90;
@@ -23,16 +45,29 @@ let paddleX = paddleW;
 let up = false;
 let down = false;
 
+// right paddle variables
+const rPaddleH = 90;
+const rPaddleW = 20;
+let rPaddleY = (cv.height - rPaddleH) / 2;
+let rPaddleX = cv.width - (rPaddleW * 2);
+let rup = false;
+let rdown = false;
+
 let interval = 0;
 
 function drawPaddle()
 {
+	if (rup)
+		rPaddleY = Math.max(rPaddleY - 5, 0);
+	if (rdown)
+		rPaddleY = Math.min(rPaddleY + 5, cv.height - rPaddleH);
 	if (up)
 		paddleY = Math.max(paddleY - 5, 0);
 	if (down)
 		paddleY = Math.min(paddleY + 5, cv.height - paddleH);
 	context.beginPath();
 	context.rect(paddleX, paddleY, paddleW, paddleH);
+	context.rect(rPaddleX, rPaddleY, rPaddleW, rPaddleH);
 	context.fillStyle = 'white';
 	context.fill();
 	context.closePath();
@@ -43,7 +78,10 @@ function draw()
 	context.clearRect(0, 0, cv.width, cv.height);
 	context.fillStyle = 'black';
 	context.fillRect(0, 0, cv.width, cv.height);
-	if (y >= paddleY - paddleH && y <= paddleY + paddleH  && (x + dX) - r < paddleX + paddleW)
+	// draw center vertical line
+	context.fillStyle = 'white';
+	context.fillRect(cv.width / 2 - 10, 0, 10, cv.height);
+	if ((y >= paddleY - paddleH && y <= paddleY + paddleH  && (x + dX) - r < paddleX + paddleW) || y >= rPaddleY - rPaddleH && y <= rPaddleY + rPaddleH && (x + dX) + r > rPaddleX)
 	{
 		console.log('touched');
 		style = 'teal';
@@ -65,6 +103,10 @@ function draw()
 
 function keyDownHandler(event)
 {
+	if (event.key == 'ArrowUp')
+		rup = true;
+	else if (event.key == 'ArrowDown')
+		rdown = true;
 	if (event.key == 'w')
 		up = true;
 	else if (event.key == 's')
@@ -73,6 +115,10 @@ function keyDownHandler(event)
 
 function keyUpHandler(event)
 {
+	if (event.key == 'ArrowUp')
+		rup = false;
+	else if (event.key == 'ArrowDown')
+		rdown = false;
 	if (event.key == 'w')
 		up = false;
 	if (event.key == 's')
