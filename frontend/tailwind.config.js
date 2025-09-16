6ecbc4692c68dd5c94f521c25d384fa0700f7337
Piolo <piolodale@gmail.com>
2025-09-16 17:25:32 +0400

    Added new font, changed index to be a start page and changed old index to game page, commented node_modules removal in makefile

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,19 @@
 export const content = [
   "./index.html",
-  "./src/**/*.{ts,tsx}"
+  // "./src/**/*.{ts,tsx}"
+  "./*.{ts,tsx}"
 ]
+
+// module.exports = {
+//   content: content,
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         pixelgame: ['PixelGame', 'sans-serif'],
+//         boldpixels: ['BoldPixels', 'mono'],
+//         montserrat: ['Montserrat', 'sans-serif'],
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
