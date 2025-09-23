fa3b27d705458fd9938fab4240decb4351b59d09
Pierce Polintan <pipolint@lab1r1s5.42abudhabi.ae>
2025-09-23 20:16:51 +0400

    made change to game typescript file

--- frontend/app/src/game.ts
+++ frontend/app/src/game.ts
@@ -53,19 +53,11 @@ class Paddle
 	}
 };
 
-// circle variables
-let dX = 4;
-let dY = 4;
-let r = 15
-let x = r;
-let y = r;
-let style = 'white';
-const ball = new Ball(cv.width / 2, cv.height / 2, 2, 20, 'red');
+const paddle_speed = 10;
+const ball_speed = 10;
 
-// left paddle variables
+const ball = new Ball(cv.width / 2, cv.height / 2, ball_speed, 20, 'red');
 let left_paddle = new Paddle(90, 20, 20, (cv.height - 90) / 2, 'orange');
-
-// right paddle variables
 let right_paddle = new Paddle(90, 20, cv.width - (20 * 2), (cv.height - 90) / 2, 'red');
 
 let interval = 0;
@@ -73,14 +65,31 @@ let interval = 0;
 function drawPaddle(paddle: Paddle)
 {
 	if (paddle.up)
-		paddle.yPos = Math.max(paddle.yPos - 5, 0);
+		paddle.yPos = Math.max(paddle.yPos - paddle_speed, 0);
 	if (paddle.down)
-		paddle.yPos = Math.min(paddle.yPos + 5, cv.height - paddle.height);
+		paddle.yPos = Math.min(paddle.yPos + paddle_speed, cv.height - paddle.height);
 	context.beginPath();
 	context.rect(paddle.xPos, paddle.yPos, paddle.width, paddle.height);
 	context.fillStyle = paddle.color;
 	context.fill();
 	context.closePath();
+}
+
+function hit_wall(ball: Ball)
+{
+	if (ball.x + ball.speed > cv.width - ball.r)
+	{
+		ball.xVel *= -1;
+		ball.color = 'yellow';
+		return ('right');
+	}
+	if (ball.x + ball.speed < ball.r)
+	{
+		ball.xVel *= -1;
+		ball.color = 'blue';
+		return ('left');
+	}
+	return ('none');
 }
 
 function draw()
@@ -91,11 +100,19 @@ function draw()
 	// draw center vertical line
 	context.fillStyle = 'white';
 	context.fillRect(cv.width / 2 - 10, 0, 10, cv.height);
+
+	// draw the score
+	context.font = "30px SixtyFour";
+	context.textAlign = "center";
+	context.textBaseline = "top";
+	context.fillText(p1_score.innerHTML, cv.width / 4, 20);
+	context.fillText(p2_score.innerHTML, (cv.width / 4) * 3, 20);
+
 	// if the ball touched a paddle
-	if ((ball.y >= left_paddle.yPos - left_paddle.height && ball.y <= left_paddle.yPos + left_paddle.height  && (ball.x + ball.speed) - ball.r < left_paddle.xPos + left_paddle.width)
-		|| ball.y >= right_paddle.yPos - right_paddle.height && ball.y <= right_paddle.yPos + right_paddle.height && (ball.x + ball.speed) + ball.r > right_paddle.xPos)
+	if ((ball.y >= left_paddle.yPos && ball.y <= left_paddle.yPos + left_paddle.height  && (ball.x + ball.speed) - ball.r < left_paddle.xPos + left_paddle.width)
+		|| ball.y >= right_paddle.yPos && ball.y <= right_paddle.yPos + right_paddle.height && (ball.x + ball.speed) + ball.r > right_paddle.xPos)
 	{
-		style = 'pink';
+		ball.color = 'pink';
 		ball.xVel *= -1;
 		// ball.speed *= -1;
 	}
@@ -103,17 +120,29 @@ function draw()
 	if (ball.y + ball.speed > cv.height - ball.r || ball.y + ball.speed < ball.r)
 	{
 		ball.yVel *= -1;
-		style = 'yellow';
+		ball.color = 'yellow';
 	}
-	// ball is scored in either the left or right
-	if (ball.x + ball.speed > cv.width - ball.r || ball.x + ball.speed < ball.r)
+	// // ball is scored in either the left or right
+	// if (ball.x + ball.speed > cv.width - ball.r || ball.x + ball.speed < ball.r)
+	// {
+	// 	ball.xVel *= -1;
+	// 	style = 'red';
+	// }
+	const wherehit = hit_wall(ball);
+	if (wherehit == 'right')
 	{
-		ball.xVel *= -1;
-		style = 'red';
+		let current = p1_score.innerHTML;
+		p1_score.innerHTML = (parseInt(current) + 1).toString();
+	}
+	if (wherehit == 'left')
+	{
+		let current = p2_score?.innerHTML;
+		p2_score.innerHTML = (parseInt(current) + 1).toString();
 	}
 	drawPaddle(left_paddle);
 	drawPaddle(right_paddle);
 	drawBall(ball);
+	requestAnimationFrame(draw);
 	// circle(ball.x += ball.speed, ball.y += ball.speed, r);
 }
 
@@ -123,9 +152,9 @@ function keyDownHandler(event)
 		right_paddle.up = true;
 	else if (event.key == 'ArrowDown')
 		right_paddle.down = true;
-	if (event.key == 'w')
+	if (event.key == 'w' || event.key == 'W')
 		left_paddle.up = true;
-	else if (event.key == 's')
+	else if (event.key == 's' || event.key == 'S')
 		left_paddle.down = true;
 }
 
@@ -154,4 +183,5 @@ function drawBall(ball: Ball)
 
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
-interval = setInterval(draw, 10);
+draw();
+// interval = setInterval(draw, 10);
