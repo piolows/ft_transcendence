f524c1f81edac439a22fc638d862683f6e9cfc00
Pierce Polintan <pipolint@lab1r1s5.42abudhabi.ae>
2025-09-28 02:11:06 +0400

    made change to game.ts

--- frontend/app/src/game.ts
+++ frontend/app/src/game.ts
@@ -58,9 +58,9 @@ class Paddle
 };
 
 const paddle_speed = 10;
-let ball_speed = 10;
+const ball_speed = 10;
 
-const ball = new Ball(cv.width / 2, cv.height / 2, ball_speed, 20, 'red');
+const ball = new Ball(cv.width / 2, cv.height / 2, ball_speed, 15, 'white');
 const left_paddle = new Paddle(90, 20, 20, (cv.height - 90) / 2, 'orange');
 const right_paddle = new Paddle(90, 20, cv.width - (20 * 2), (cv.height - 90) / 2, 'red');
 
@@ -83,40 +83,76 @@ function hit_wall(ball: Ball)
 	if (ball.x + ball.speed > cv.width - ball.r)
 	{
 		ball.xVel *= -1;
-		ball.color = 'yellow';
+		console.log('right wall hit');
 		return ('right');
 	}
 	if (ball.x + ball.speed < ball.r)
 	{
 		ball.xVel *= -1;
-		ball.color = 'blue';
+		console.log('left wall hit');
 		return ('left');
 	}
 	return ('none');
 }
 
-function start_game(ball: Ball, left: Paddle, right: paddle)
+function resetBall(ball: Ball)
 {
-	// randomize between left and right for the ball to start heading to
-	ball.x = cv.width / 2;
+	ball.x = cv.width / 2 - ball.r / 2;
 	ball.y = cv.height / 2;
-	const randomizer = Math.floor(Math.random() * 2);
-	ball.xVel = randomizer === 0 ? ball.xVel * -1 : ball.xVel;
-	left.yPos = cv.height / 2;
-	right.yPos = cv.height / 2;
+	ball.xVel = 0;
+	ball.yVel = 0;
+	ball.speed = ball_speed;
+	setTimeout(() => {
+		const angle = (Math.random() * Math.PI / 4) - (Math.PI / 8); // -22.5° to +22.5°
+		const direction = Math.random() < 0.5 ? 1 : -1; // left or right
+		
+		ball.xVel = direction * ball.speed * Math.cos(angle);
+		ball.yVel = ball.speed * Math.sin(angle);
+	}, 1000);
 	ball.starting = false;
 }
 
-function draw()
+function hit_paddle(ball: Ball)
 {
-	if (ball.starting === true)
-		start_game(ball, left_paddle, right_paddle);
+	if (ball.y >= left_paddle.yPos && ball.y <= left_paddle.yPos + left_paddle.height  && (ball.x + ball.speed) - ball.r < left_paddle.xPos + left_paddle.width)
+	{
+		// console.log('ball hit left paddle');
+		const center = left_paddle.yPos + left_paddle.height / 2;
+		const normal_intersect = (ball.y - center) / (left_paddle.height / 2);
+		const angle = normal_intersect * (Math.PI / 4);
+		// const speed = Math.sqrt(ball.xVel**2 + ball.yVel**2);
+		ball.xVel = ball.speed * Math.cos(angle);
+		ball.yVel = ball.speed * Math.sin(angle);
+		// if (ball.first_collision != true)
+		// 	ball.first_collision = true;
+		ball.speed *= 1.05;
+		// ball.xVel *= -1;
+	}
+	if (ball.y >= right_paddle.yPos && ball.y <= right_paddle.yPos + right_paddle.height && (ball.x + ball.speed) + ball.r > right_paddle.xPos)
+	{
+		// console.log('ball hit right paddle');
+		const center = right_paddle.yPos + right_paddle.height / 2;
+		const normal_intersect = (ball.y - center) / (right_paddle.height / 2);
+		const angle = normal_intersect * (Math.PI / 4);
+		// const speed = Math.sqrt(ball.xVel**2 + ball.yVel**2); // constant speed
+		ball.xVel = ball.speed * Math.cos(angle);
+		ball.yVel = ball.speed * Math.sin(angle);
+		// if (ball.first_collision != true)
+		// 	ball.first_collision = true;
+		ball.xVel *= -1;
+		ball.speed *= 1.05;
+	}
+}
+
+function draw_bg()
+{
+	const middle_line_width = 5;
 	context.clearRect(0, 0, cv.width, cv.height);
 	context.fillStyle = 'black';
 	context.fillRect(0, 0, cv.width, cv.height);
 	// draw center vertical line
 	context.fillStyle = 'white';
-	context.fillRect(cv.width / 2 - 10, 0, 10, cv.height);
+	context.fillRect(cv.width / 2, 0, middle_line_width, cv.height);
 
 	// draw the score
 	context.font = "30px SixtyFour";
@@ -125,34 +161,38 @@ function draw()
 	context.fillText(p1_score.innerHTML, cv.width / 4, 20);
 	context.fillText(p2_score.innerHTML, (cv.width / 4) * 3, 20);
 
-	// if the ball touched a paddle
-	if ((ball.y >= left_paddle.yPos && ball.y <= left_paddle.yPos + left_paddle.height  && (ball.x + ball.speed) - ball.r < left_paddle.xPos + left_paddle.width)
-		|| ball.y >= right_paddle.yPos && ball.y <= right_paddle.yPos + right_paddle.height && (ball.x + ball.speed) + ball.r > right_paddle.xPos)
-	{
-		if (ball.first_collision != true)
-			ball.first_collision = true;
-		ball.color = 'pink';
-		ball.xVel *= -1;
-	}
-	// ball hit the top or bottom wall
-	if (ball.y + ball.speed > cv.height - ball.r || ball.y + ball.speed < ball.r)
-	{
+	context.fillStyle = 'white';
+	context.beginPath();
+	context.arc(cv.width / 2 + 5, cv.height / 2, 100, 0, Math.PI * 2);
+	context.strokeStyle = 'white';
+	context.lineWidth = 5;
+	context.stroke();
+	context.closePath();
+}
+
+function draw()
+{
+	if (ball.starting === true)
+		resetBall(ball);
+	draw_bg();
+	hit_paddle(ball);	// if the ball touched a paddle
+	if (ball.y + ball.speed > cv.height - ball.r || ball.y + ball.speed < ball.r)	// ball hit the top or bottom boundaries
 		ball.yVel *= -1;
-		ball.color = 'yellow';
-	}
 	const wherehit = hit_wall(ball);
 	if (wherehit == 'right')
 	{
 		let current = p1_score.innerHTML;
 		p1_score.innerHTML = (parseInt(current) + 1).toString();
-		start_game(ball, left_paddle, right_paddle);
+		// start_game(ball, left_paddle, right_paddle);
+		resetBall(ball);
 		ball.first_collision = false;
 	}
 	if (wherehit == 'left')
 	{
 		let current = p2_score?.innerHTML;
 		p2_score.innerHTML = (parseInt(current) + 1).toString();
-		start_game(ball, left_paddle, right_paddle);
+		// start_game(ball, left_paddle, right_paddle);
+		resetBall(ball);
 		ball.first_collision = false;
 	}
 	drawPaddle(left_paddle);
@@ -179,28 +219,22 @@ function keyUpHandler(event)
 		right_paddle.up = false;
 	else if (event.key == 'ArrowDown')
 		right_paddle.down = false;
-	if (event.key == 'w')
+	if (event.key == 'w' || event.key == 'W')
 		left_paddle.up = false;
-	if (event.key == 's')
+	if (event.key == 's' || event.key == 'S')
 		left_paddle.down = false;
 }
 
 function drawBall(ball: Ball)
 {
 	context.beginPath();
-	context.arc(ball.x, ball.y, ball.r, 0, Math.PI * 2);
+	// context.arc(ball.x, ball.y, ball.r, 0, Math.PI * 2);
 	context.fillStyle = ball.color;
+	context.fillRect(ball.x, ball.y, ball.r, ball.r);
 	context.fill();
 	context.closePath();
-	if (ball.first_collision === false)
-	{
-		ball.x += ball.xVel;
-	}
-	else
-		ball.x += ball.xVel;
-	if (ball.first_collision === true)
-		ball.y += ball.yVel;
-	// ball_speed += 0.1;
+	ball.x += ball.xVel;
+	ball.y += ball.yVel;
 }
 
 document.addEventListener('keydown', keyDownHandler, false);
