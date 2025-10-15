925075ad1bead6994eca17286e85ae24e8551a32
EmadHammoudeEJ <emad.hammoude@camb.ai>
2025-10-15 18:20:51 +0400

    Implemented server-side pong (untested). Moved classes to separate file from fastify socket handler

--- Makefile
+++ Makefile
@@ -10,15 +10,15 @@ clean:
 	docker compose down --volumes --remove-orphans
 
 fclean: clean
-#	 rm -rf ./backend/auth/app/src/node_modules
-#	 rm -rf ./backend/controller/app/src/node_modules
-#	 rm -rf ./backend/games/app/src/node_modules
-#	 rm -rf ./backend/users/app/src/node_modules
-#	 rm -rf ./frontend/app/src/node_modules
 	docker compose down --rmi all --remove-orphans
 	docker volume rm $$(docker volume ls -q) || true
 	docker system prune --all --force
 	docker volume prune -f
+#	 rm -rf ./backend/controller/app/src/node_modules
+#	 rm -rf ./backend/games/app/src/node_modules
+#	 rm -rf ./backend/users/app/src/node_modules
+#	 rm -rf ./backend/auth/app/src/node_modules
+#	 rm -rf ./frontend/app/src/node_modules
 
 re: fclean all
 
--- backend/games/app/src/pong.controller.js
+++ backend/games/app/src/pong.controller.js
@@ -1,125 +1,32 @@
+import { Member, Game } from "./pong_classes.js";
+
 const pongHandler = (fastify, options, done) => {
-	class Member {
-		id;
-		user_info;
-		socket;
-		game = null;
-		is_player = false;
-
-		constructor(socket, user_info) {
-			this.id = crypto.randomUUID();
-			this.socket = socket;
-			this.user_info = user_info;
-		}
-
-		join(game) {
-			if (this.game)
-				this.leave();
-			game.join(this);
-		}
-
-		play(game) {
-			if (this.game)
-				this.leave();
-			game.player_join(this);
-		}
-
-		spec(game) {
-			if (this.game)
-				this.leave();
-			game.spec_join(this);
-		}
-
-		leave() {
-			if (!this.game) {
-				throw new Error("User not in a game");
-			}
-			if (this.is_player)
-				this.game.player_leave(this);
-			else
-				this.game.spec_leave(this);
-		}
-	}
-
-	class Game {
-		id;
-		admin;       //cookie info (id, username, email, avatarURL)
-		players = {};
-		specs = {};
-		all = {};
-		done = false;
-		p1_score = 0;
-		p2_score = 0;
-
-		constructor(admin) {
-			this.id = crypto.randomUUID();
-			this.admin = admin;
-		}
-
-		player_count() {
-			return Object.keys(this.players).length;
-		}
-
-		spec_count() {
-			return Object.keys(this.specs).length;
-		}
-
-		join(member) {
-			if (this.players[member.user_info.username] || this.specs[member.user_info.username])
-				throw new Error("User already in game");
-			if (this.player_count() == 2)
-				this.spec_join(member);
-			else
-				this.player_join(member);
-		}
-
-		player_join(player) {
-			if (this.players[player.user_info.username])
-				throw new Error("Player already in game");
-			if (this.player_count() == 2)
-				throw new Error("Max players in current game");
-			player.is_player = true;
-			player.game = this;
-			this.players[player.user_info.username] = player;
-			delete this.specs[player.user_info.username];
-			this.all[player.user_info.username] = player;
-		}
-
-		player_leave(player) {
-			if (!this.players[player.user_info.username])
-				throw new Error("Player not in this game");
-			this.players[player.user_info.username].game = null;
-			this.players[player.user_info.username].is_player = false;
-			delete this.players[player.user_info.username];
-			delete this.all[player.user_info.username];
-		}
-
-		spec_join(spec) {
-			spec.is_player = false;
-			this.specs[spec.user_info.username] = spec;
-			this.all[spec.user_info.username] = spec;
-		}
-
-		spec_leave(spec) {
-			if (!this.specs[spec.user_info.username])
-				throw new Error("User not in this game");
-			delete this.specs[spec.user_info.username];
-			delete this.all[spec.user_info.username];
-		}
-	}
-
-	let games = {};
+	let games = {};		//game.uuid -> game
+	let admins = {};	//username -> game
 
 	fastify.post("/new", (req, resp) => {
-		if (Object.keys(games).length > 1024) {
+		if (Object.keys(games).length > 1024)
 			return resp.code(501).send({ status: "failed", error: "Maximum game limit reached" });
-		}
-		if (!req.session || !req.session.user) {
+		if (!req.session || !req.session.user)
 			return resp.code(403).send({ error: "Must be signed in to create a game" });
-		}
+		if (admins[req.session.user.username])
+			return resp.code(403).send({ error: "User already has an open room" });
 		const game = new Game(req.session.user);
-		games[game.id] = game;
-		return resp.code(200).send({ status: "success", game_id: game.id})
+		games[game.uuid] = game;
+		admins[req.session.user.username] = game;
+		console.log(`${req.session.user.username} created room ${game.uuid}`);
+		return resp.code(200).send({ status: "success", game_id: game.uuid});
+	});
+
+	fastify.post("/destroy", (req, resp) => {
+		if (Object.keys(games).length > 1024)
+			return resp.code(501).send({ status: "failed", error: "Maximum game limit reached" });
+		if (!req.session || !req.session.user)
+			return resp.code(403).send({ error: "Must be signed in to create a game" });
+		if (admins[req.session.user.username])
+			console.log(`Destroyed room ${admins[req.session.user.username].uuid}`);
+		delete admins[req.session.user.username];
+		return resp.code(200).send({ status: "success" });
 	});
 
 	fastify.get("/", {
@@ -134,7 +41,7 @@ const pongHandler = (fastify, options, done) => {
 		}, (socket, req) => {
 			const member = new Member(socket, req.session.user);
 
-			console.log(`New player connected: ${member.id}`);
+			console.log(`New user connected to socket: ${member.uuid} - ${member.user_info.username} - ${member.user_info.email}`);
 
 			socket.on('message', (msg) => {
 				let data;
@@ -147,14 +54,17 @@ const pongHandler = (fastify, options, done) => {
 
 				const { game_id, action, param } = data;
 
-				const getinfo = (guy) => {return `${guy.id} - ${guy.email} - ${guy.username}`;}
+				//============ TEMPORARY FOR TESTING =============//
+				const getinfo = (guy) => {return `${guy.email} - ${guy.username}\n`;}
 				const getinfos = (guys) => {let ret = ""; for (const guy of guys) ret += getinfo(guy.user_info) + "  "; return ret;}
 				if (action == "INFO") {
 					for (const game of Object.values(games)) {
-						socket.send(`Game of ID #${game.id} has admin ${getinfo(game.admin)} with ${getinfos(Object.values(game.specs))} spectators and ${getinfos(Object.values(game.players))} players.`);
+						socket.send(`Game #${game.uuid} has admin ${getinfo(game.admin)}.\nSpectators:\n${getinfos(Object.values(game.specs))}\nPlayers:\n${getinfos(Object.values(game.players))}`);
+						console.log(`Game #${game.uuid} has admin ${getinfo(game.admin)}.\nSpectators:\n${getinfos(Object.values(game.specs))}\nPlayers:\n${getinfos(Object.values(game.players))}`);
 					};
 					return;
 				}
+				//============ TEMPORARY FOR TESTING =============//
 
 				if (!game_id || !action) {
 					socket.send(JSON.stringify({ error: "Invalid message: Must container game_id & action [& param]" }));
@@ -164,28 +74,40 @@ const pongHandler = (fastify, options, done) => {
 					socket.send(JSON.stringify({ error: "Game not found" }));
 					return;
 				}
-
 				try {
 					switch (action) {
-						case "PLAY":
-							member.play(games[game_id]);
-							socket.send("Joined game #" + game_id);
-							return;
-						case "SPEC":
-							member.spec(games[game_id]);
-							socket.send("Joined game #" + game_id);
-							return;
 						case "JOIN":
-							member.join(games[game_id]);
-							socket.send("Joined game #" + game_id);
+							if (param && param != "PLAY" && param != "SPEC") {
+								socket.send(JSON.stringify({ error: "Invalid message param: JOIN only takes PLAY/SPEC as optional params." }));
+								return;
+							}
+							if (param && param == "PLAY") {
+								member.play(games[game_id]);
+								socket.send("Joined game #" + game_id);
+							}
+							else if (param && param == "SPEC") {
+								member.spec(games[game_id]);
+								socket.send("Joined game #" + game_id);
+							}
+							else {
+								member.join(games[game_id]);
+								socket.send("Joined game #" + game_id);
+							}
+							console.log(`User ${member.user_info.username} - ${member.user_info.email} joined game #${game_id}`);
 							return;
 						case "LEAVE":
-							const tmp_game = member.game;
-							member.leave();
-							if (tmp_game.player_count() == 0) {
-								delete games[tmp_game.id];
+							const gid = member.game.uuid;
+							if (gid != game_id) {
+								socket.send(JSON.stringify({ error: `Wrong game ID. User is in game #${gid}` }));
+								return;
 							}
-							socket.send("Left game #" + game_id);
+							member.leave();
+							console.log(`User ${member.user_info.username} - ${member.user_info.email} left game #${gid}`);
+							if (games[gid].player_count() == 0) {
+								delete games[gid];
+								console.log(`Destroyed room ${gid}`);
+							}
+							socket.send("Left game #" + gid);
 							return;
 						case "MOVE_UP":
 							
@@ -207,7 +129,7 @@ const pongHandler = (fastify, options, done) => {
 							socket.send("You said: " + param);
 							return;
 						default:
-							socket.send(JSON.stringify({ error: "Invalid action. Available actions: PLAY/SPEC/JOIN/LEAVE/MOVE_UP/MOVE_DOWN/STOP/MESSAGE" }));
+							socket.send(JSON.stringify({ error: "Invalid action. Available actions: JOIN/LEAVE/MOVE_UP/MOVE_DOWN/STOP/MESSAGE" }));
 							return;
 					}
 				} catch (err) {
@@ -218,10 +140,11 @@ const pongHandler = (fastify, options, done) => {
 
 			socket.on('close', () => {
 				if (member.game) {
-					const id = member.game.id;
+					const id = member.game.uuid;
 					member.leave();
 					if (games[id].player_count() == 0) {
 						delete games[id];
+						console.log(`Destroyed room ${id}`);
 					}
 				}
 				return;
